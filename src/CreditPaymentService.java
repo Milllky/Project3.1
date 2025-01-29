38e16ea9212d8013923477653986b5fb0f9c7fda
Milllky <milllky@rocketmail.com>
2025-01-29 16:13:52 +0500

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public int calculate(int sum, int time, double per) {
-        double monthPercent = (per / (100 * time));
+        double monthPercent = (per / (100 * 12));
         double x = (1 - (Math.pow((1 + monthPercent), -time)));
         int result = (int) (sum * (monthPercent / x));
         return result;
